605f2e4c05ebb5b9f934709457c89cf306e31377
Rinat Nadav <ringhost@gmail.com>
2020-08-14 14:42:55 +0200

    added google auth component and changed header style

---
+++ src/components/GoogleAuth.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component{
+
+    render(){
+        return(
+            <div>
+                google auth
+            </div>
+        )
+    }
+}
+export default GoogleAuth;
+
+//1087217222329-u2qejiic5mk9c7up9q2844ievgsmvvb8.apps.googleusercontent.com
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import GoogleAuth from './GoogleAuth';
 import {Link} from 'react-router-dom';
 const Header = () =>{
     return (
-        <div className="ui secondary pointing menu">
-            <div className="left menu">
+        <div className="ui labeled icon menu">
+            <div className="item left menu">
                 <Link to="/" className="item">
+                <i className="video camera red icon"></i>
                     Streamy
                 </Link>
             </div>
-            <div className="right menu">
+            <div className="item right menu">
+            
                 <Link to="/" className="item">
+                <i className="file video blue icon"></i>
                     All Streams
                 </Link>
+                <GoogleAuth/>
             </div>
         </div>
     )
 };
 
 export default Header;
+
+// 1087217222329-u2qejiic5mk9c7up9q2844ievgsmvvb8.apps.googleusercontent.com
