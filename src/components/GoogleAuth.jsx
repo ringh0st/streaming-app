db59be8d8059676afba06cc09ab4cacd658e1d0f
Rinat Nadav <ringhost@gmail.com>
2020-08-17 23:08:40 +0000

    added authReducer

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {signIn,signOut} from '../actions'
-class GoogleAuth extends React.Component{
-    state={
-        isSignedIn:null,
-        userName:null,
-        userProfileImgUrl:null,
+import { signIn, signOut } from '../actions'
+class GoogleAuth extends React.Component {
+    state = {
+        isSignedIn: null,
+        userName: null,
+        userProfileImgUrl: null,
     }
-    componentDidMount(){
-        window.gapi.load('client:auth2',()=>{
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
 
             //load is followed by a callback function. 
             //init doesnt need CB function cause its a promise followed by function then()
@@ -17,49 +17,60 @@ class GoogleAuth extends React.Component{
                     '1087217222329-u2qejiic5mk9c7up9q2844ievgsmvvb8.apps.googleusercontent.com',
                 scope:
                     'profile',
-            }).then(()=>{
+            }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({
-                    isSignedIn:this.auth.isSignedIn.get(),
+                    isSignedIn: this.auth.isSignedIn.get(),
                 })
                 this.auth.isSignedIn.listen(this.onAuthChange);
                 console.log(this.auth.currentUser.get().getBasicProfile());
             })
         })
     }
-    onAuthChange =()=>{
-        this.setState({isSignedIn:this.auth.isSignedIn.get()})
+    onAuthChange = (isSignedIn) => {
+        // this.setState({isSignedIn:this.auth.isSignedIn.get()})
+        if (isSignedIn) {
+            this.props.signIn()
+        } else {
+            this.props.signOut()
+        }
     }
-    onSignInClick = () =>{
+    onSignInClick = () => {
         this.auth.signIn()
-        this.setState({                    
-            userName:this.auth.currentUser.get().getBasicProfile().getName(),
-            userProfileImgUrl:this.auth.currentUser.get().getBasicProfile().getImageUrl()})
+        this.setState({
+            userName: this.auth.currentUser.get().getBasicProfile().getName(),
+            userProfileImgUrl: this.auth.currentUser.get().getBasicProfile().getImageUrl()
+        })
     }
-    onSignOutClick = () =>{
-        this.auth.signOut() 
+    onSignOutClick = () => {
+        this.auth.signOut()
     }
-    renderAuthButton(){
-        if(this.state.isSignedIn === null){
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
             return <div>I dont know if we are signed in</div>
-        } else if (this.state.isSignedIn){
+        } else if (this.state.isSignedIn) {
             return <>
-            <h2>            
-                <img style={{width:'40px'}} src={this.state.userProfileImgUrl} alt={this.state.userProfileImgUrl}/>
-                Hello {this.state.userName} 
-            </h2>
-            <button onClick={this.onSignOutClick} className="ui red google button">
-                <i className="google icon"/>Sign Out
+                <div className="ui card">
+                    <div className="image">
+                        <img style={{ width: '40px' }} src={this.state.userProfileImgUrl} alt={this.state.userProfileImgUrl} />
+                    </div>
+                    <div className="content">
+                        Hello {this.state.userName}
+
+                    </div>
+                </div>
+                <button onClick={this.onSignOutClick} className="ui red google button">
+                    <i className="google icon" />Sign Out
             </button></>
-        } else{
+        } else {
             return <button onClick={this.onSignInClick} className="ui red google button">
-                <i className="google icon"/>Sign In With Google
+                <i className="google icon" />Sign In With Google
             </button>
         }
-        
+
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 {this.renderAuthButton()}
             </div>
@@ -68,7 +79,7 @@ class GoogleAuth extends React.Component{
 }
 export default connect(
     null,
-    {signIn,signOut})
+    { signIn, signOut })
     (GoogleAuth);
 
 //1087217222329-u2qejiic5mk9c7up9q2844ievgsmvvb8.apps.googleusercontent.com
