2d8ff2374271dcd9a1fdd3e008a17f53d26ef786
Rinat Nadav <ringhost@gmail.com>
2020-08-20 22:27:23 -0100

    edited StreamCreate to make a redux form

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () =>{
-    return <div>StreamCreate</div>
+class StreamCreate extends React.Component{
+    renderInput(formProps){
+        console.log(formProps);
+        return(
+            <input/>
+        )
+    }
+    render(){
+        console.log(this.props);
+        return <form>
+            <Field name="title" component={this.renderInput}/>
+            <Field name="description" component={this.renderInput}/>
+        </form>
+
+    }
 };
 
-export default StreamCreate;
+export default reduxForm({
+    form:'streamCreate'
+})(StreamCreate);
