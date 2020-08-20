dfd6b126d44da44980b80c70724c99dc75fef343
Rinat Nadav <ringhost@gmail.com>
2020-08-20 22:41:47 -0100

    styled streamCreate form with semantic ui

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component{
-    renderInput({input}){
+    renderInput({input,lable}){
         return(
-            <input {...input}
-            // onChange={formProps.input.onChange} 
-            // value={formProps.input.value}
-            // instead we should destruct input 
-            />
+            <div className="field">
+                <label>{lable}</label>
+                <input {...input}/>
+            </div>
+            
         )
     }
+                // onChange={formProps.input.onChange} 
+            // value={formProps.input.value}
+            // instead we should destruct input 
     render(){
         console.log(this.props);
-        return <form>
-            <Field name="title" component={this.renderInput}/>
-            <Field name="description" component={this.renderInput}/>
+        return <form className="ui form">
+            <Field name="title" component={this.renderInput} lable="Enter Title"/>
+            <Field name="description" component={this.renderInput} lable ="Enter Description"/>
         </form>
 
     }
