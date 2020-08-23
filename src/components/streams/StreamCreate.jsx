52a603489b5da081554ee01244a6b5148e64ae6c
Rinat Nadav <ringhost@gmail.com>
2020-08-22 21:22:13 -0300

    added a submit button

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-class StreamCreate extends React.Component{
-    renderInput({input,lable}){
-        return(
+class StreamCreate extends React.Component {
+    renderInput({ input, lable }) {
+        return (
             <div className="field">
                 <label>{lable}</label>
-                <input {...input}/>
+                <input {...input} />
             </div>
-            
+
         )
     }
-                // onChange={formProps.input.onChange} 
-            // value={formProps.input.value}
-            // instead we should destruct input 
-    render(){
+    onSubmit(formValues) {
+        console.log(formValues)
+        // e.preventDefault()
+    }
+    // onChange={formProps.input.onChange} 
+    // value={formProps.input.value}
+    // instead we should destruct input 
+    render() {
         console.log(this.props);
-        return <form className="ui form">
-            <Field name="title" component={this.renderInput} lable="Enter Title"/>
-            <Field name="description" component={this.renderInput} lable ="Enter Description"/>
+        return <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+            <Field
+                name="title"
+                component={this.renderInput}
+                lable="Enter Title"
+            />
+            <Field
+                name="description"
+                component={this.renderInput}
+                lable="Enter Description"
+            />
+            <button className="ui button primary">submit</button>
         </form>
 
     }
 };
 
 export default reduxForm({
-    form:'streamCreate'
+    form: 'streamCreate'
 })(StreamCreate);
