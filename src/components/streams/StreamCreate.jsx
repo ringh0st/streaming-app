3d5cab351c763a877cc0e17f61d7b1ab06aa8035
Rinat Nadav <ringhost@gmail.com>
2020-08-23 20:45:02 -0200

    added a validation to the form

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm, formValues } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput({ input, lable }) {
+    renderInput({ input, lable , meta}) {
         return (
             <div className="field">
                 <label>{lable}</label>
                 <input {...input} />
+                <div>{meta.error}</div>
             </div>
 
         )
@@ -19,7 +20,7 @@ class StreamCreate extends React.Component {
     // value={formProps.input.value}
     // instead we should destruct input 
     render() {
-        console.log(this.props);
+        // console.log(this.props);
         return <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
             <Field
                 name="title"
@@ -36,7 +37,19 @@ class StreamCreate extends React.Component {
 
     }
 };
-
+const validate = (formValues) =>{
+    const error={}
+    if(!formValues.title){
+        //only ran if the user did not enter a title
+        error.title = 'You must enter a title'
+    }
+    if(!formValues.description){
+        //only ran if the user did not enter a description
+        error.description = 'You must enter a description'
+    }
+    return error;
+}
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate);
