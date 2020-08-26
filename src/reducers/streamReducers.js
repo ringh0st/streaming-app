5360ff7279da7ecd6cd7d1e2a08e1d88b727e68a
Rinat Nadav <ringhost@gmail.com>
2020-08-26 22:00:45 +0300

    added streamReducers

---
+++ src/reducers/streamReducers.js
@@ -0,0 +1,17 @@
+import {
+    CREATE_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    DELETE_STREAM,
+    EDIT_STREAM
+
+} from '../actions/types';
+
+export default (state={}, action)=>{
+    switch(action.type){
+        case FETCH_STREAM:
+            return{...state, [action.payload.id]: action.payload}
+        default:
+            return state;
+    }
+};
